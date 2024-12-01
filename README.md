81b72ac2d34ed1c3957fa930e696760f9130293c
Félix Defrance <felixgif@gmail.com>
2024-12-02 00:46:52 +0100

    adds day 1 and init deno project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-solving Advent of Code 2024 problems with deno to learn more about it
+# 🎄 Advent of Code 2024
+
+**Solving Advent of Code 2024 problems with Typescript and Deno!**
+
+[(this repo doesn't include puzzles inputs)](https://adventofcode.com/2024/about#faq_copying)
---
+++ day_01/main.ts
@@ -0,0 +1,22 @@
+const data = await Deno.readTextFile('./day_01/input.txt');
+
+const rows = data.trim().split('\n').map((row) =>
+	row.trim().split(/\s+/).map(Number)
+);
+
+const [firstColumn, secondColumn] = [
+	rows.map((row) => row[0]).sort(),
+	rows.map((row) => row[1]).sort(),
+];
+
+// Part 1
+console.log(firstColumn.reduce(
+	(sum, val, i) => sum + Math.abs(val - secondColumn[i]),
+	0,
+));
+
+// Part 2
+console.log(firstColumn.reduce(
+	(sum, val) => sum + val * secondColumn.filter((x) => x === val).length,
+	0,
+));
